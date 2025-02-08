946c454cc5c6a8365acf5c647355a1182dbd8a8e
Pratik Erande <pratikerande73@gmail.com>
2025-02-08 21:00:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,33 @@
-let start = document.querySelector(".start");
-let stop = document.querySelector(".stop");
-let reset = document.querySelector(".reset");
-let hours = document.querySelector(".hour");
-let mins = document.querySelector(".min");
-let secs = document.querySelector(".sec");
+$(document).ready(function(){
 
-let hour = 0;
-let min = 0;
-let sec = 0;
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
 
-let interval;
+    $(window).on('scroll load',function(){
 
-start.addEventListener("click" , function(){
-
-    start.classList.add("active");
-    stop.classList.remove("active");
-    interval = setInterval(function(){
-
-        if(sec < 59){
-            sec +=1;
-            secs.innerHTML = sec < 10 ? "0" +sec : sec;
-
+        $('#menu').removeClass('fa-times');
+        $('#header').removeClass('toggle');
+        
+        if($(window).scrollTop() > 0){
+            $('.top').show();
         }else{
-            sec = 0;
-            secs.innerHTML = sec <10 ? "0" + sec : sec;
-
-            if (min < 59){
-                min += 1;
-                mins.innerHTML = min < 10 ? "0" + min + ":" :min + ":";
-            }else{
-                hour += 1;
-                hours.innerHTML = hour < 10 ? "0" + hour + ":" :hour + ":";
-            }
+            $('.top').hide();
         }
-    },1000);
-    start.style.pointerEvent = "none";
-    stop.style.pointerEvent = "auto";
+    });
+
+    $('a[herf*="#"]').on('click',function(e){
+
+        e.preventDefault();
+
+        $('html,body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+    );
+    });
 });
-
-stop.addEventListener("click",function(){
-
-    stop.classList.add("active");
-    start.classList.remove("active");
-
-
-    clearInterval(interval);
-    start.style.pointerEvent = "auto";
-    stop.style.pointerEvent = "none";
-});
-
-reset.addEventListener("click",function(){
-
-    reset.classList.add("active");
-    stop.classList.remove("active");
-    start.classList.remove("active");
-
-    location.reload();
-})
 
